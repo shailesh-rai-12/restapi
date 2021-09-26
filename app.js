4d028e5ea5cfa91109a3d55b6355863416f6b12a
Shailesh <shaileshrai1001@gmail.com>
2021-09-26 16:54:44 +0530

    cors policy fixed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 const mongoose= require('mongoose')
+
+
 //acccessing env
 require('dotenv/config');
 
@@ -10,6 +12,23 @@ const routes=require('./routes/employees');
 
 //body parser
 app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if(req.method === 'OPTIONS')
+  {
+    res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+
+  }
+  next();
+
+});
 
 app.use('/',routes);
 
